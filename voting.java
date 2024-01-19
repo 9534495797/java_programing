8733929892cac23e2bdcceb44bc673cff6faf836
9534495797 <chaubeyankit927@gmail.com>
2024-01-19 18:18:09 +0530

    ok

---
+++ voting.java
@@ -0,0 +1,11 @@
+public class voting{
+public static void main(String []args){
+int x=15;
+if(x>=18){
+System.out.println("eligible for vote in india.");
+}
+else{
+System.out.println(" not eligible for vote in india.");
+}
+}
+}
