ac3ca9fdc67681765d0ad7dde23fefa88e586ac5
9534495797 <chaubeyankit927@gmail.com>
2024-02-06 18:21:02 +0530

    ok

---
+++ Ankit.java
@@ -0,0 +1,23 @@
+// public class Ankit{
+//     public static void main(String[]args){
+//         System.out.println("hello world!");
+//     }
+// }
+
+import java.util.Scanner;
+
+public class Ankit{
+    public static void main(String[]args){
+        Scanner sc= new Scanner(System.in);
+        int z=sc.nextInt();
+    int x=0;
+    int y=1;
+        for(int i=0;i<=z;i++){
+x=y;
+int w=x+y;
+y=w;
+System.out.println(w); 
+        }
+          
+    }
+}
