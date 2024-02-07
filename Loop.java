b044268ffe0304c275c5c68f19f3af9cd45f6061
9534495797 <chaubeyankit927@gmail.com>
2024-02-07 08:17:01 +0530

    ok'

---
+++ Loop.java
@@ -0,0 +1,7 @@
+public class Loop {
+    public static void main(String[]args){
+        for(int i,j=5; i>0 && j>2;j++,j--){
+            System.out.println("i is: "+i+"and j is: "+j);
+        }
+    }
+}
