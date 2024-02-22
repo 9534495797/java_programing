5f6f467d9df035c7608baef480cfe80acb7e9e1d
9534495797 <chaubeyankit927@gmail.com>
2024-02-22 06:29:36 +0530

    ok

---
+++ sumarray.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class sumarray {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        // int[]arr={1,2,3,4,5,6};
+      
+        int n=sc.nextInt();
+        int[]arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+//         int sum=0;
+//         for(int i=0;i<=arr.length;i++){
+//             sum+=i;
+//         }
+// System.out.println(sum);
+//to find largest elemenet in array
+int maximum=arr[0];
+
+for(int i=1;i<arr.length;i++){
+    if(arr[i]<maximum){
+        maximum=arr[i];
+    
+   }
+ 
+}
+System.out.println(maximum);
+    }
+}
